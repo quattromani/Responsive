9af941cee4d882aa78bcefcff72ec61051ea17aa
Noel Killebrew <noel.killebrew@placewise.com>
2015-02-17 16:03:02 -0700

    Dells: still going through QA mainly inside page styles here

--- outlets-dells/dist/js/build/production.js
+++ outlets-dells/dist/js/build/production.js
@@ -7,29 +7,21 @@ $(function() {
   $('li:last-child').addClass('js-last');
 });
 ;// Toggle Navigation
-(function($) {
-
-  $.fn.toggleNav = function() {
-    $('.open-panel').click(function() {
-      if($('html').hasClass('open-nav')) {
-        $('html').removeClass('open-nav');
-      } else {
-        $('html').addClass('open-nav');
-      }
-
-      $(this).toggleClass('active');
-    });
-
-    $('.close-panel').click(function() {
-      if($('html').hasClass('open-nav')) {
-        $('html').removeClass('open-nav');
-      }
-    });
-  }
-
-}(jQuery));
-
-$('nav').toggleNav();;/** @preserve
+$(function() {
+  $(".open-panel").click(function(){
+    if($('html').hasClass('open-nav')) {
+      $('html').removeClass('open-nav');
+    } else {
+      $('html').addClass('open-nav');
+    }
+    $(this).toggleClass('active');
+  });
+  $('.close-panel').click(function() {
+    if($('html').hasClass('open-nav')) {
+      $('html').removeClass('open-nav');
+    }
+  });
+});;/** @preserve
  *
  * slippry v1.2.7 - Responsive content slider for jQuery
  * http://slippry.com
